18d91f856b0dc6196d537668632c87ecae98c624
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-04-03 20:31:15 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "captain",
+    products: [
+        .executable(name: "captain", targets: ["captain"]),
+        .library(name: "CaptainKit", targets: ["CaptainKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Carthage/Commandant.git", from: "0.15.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.2"),
+    ],
+    targets: [
+        .target(name: "captain", dependencies: ["Commandant", "CaptainKit"]),
+        .target(name: "CaptainKit", dependencies: ["Yams"]),
+    ]
+)
---
+++ Sources/captain/main.swift
@@ -0,0 +1,3 @@
+import Commandant
+
+print("this is a test")
