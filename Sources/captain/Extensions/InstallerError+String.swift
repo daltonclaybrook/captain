cf8cad5d3ef67ec3c19d26a82019acb12e789dda
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-04-07 20:38:17 -0400

    Now installing git hook scripts in hooks folder

--- Sources/captain/Extensions/InstallerError+String.swift
+++ Sources/captain/Extensions/InstallerError+String.swift
@@ -1,5 +1,6 @@
 import CaptainKit
 
+//swiftlint:disable line_length
 extension InstallerError: CustomStringConvertible {
     public var description: String {
         switch self {
@@ -11,6 +12,10 @@ extension InstallerError: CustomStringConvertible {
             return "No config file found at specified path"
         case .failedToParseConfig:
             return "Failed to parse config file"
+        case .gitHookExists(let hook):
+            return "An existing unmanaged git hook was found (\(hook.rawValue)). Remove this hook manually, or re-run with --force."
+        case .failedToInstallGitHook:
+            return "An unknown error occurred when trying to install git hooks"
         }
     }
 }
